9be24afdf9ea2f6002e8a1c6a367a3d343db21a8
telefon2232 <telefon2232@gmail.com>
2020-07-13 17:20:57 +0300

    added check validate character input

--- bot.py
+++ bot.py
@@ -26,12 +26,9 @@ random_number_message = 10 ** 100
 
 
 db_len = 14
-#Монстры персонажей
-mobs = {
-}
-
 #Персонажи
 characters={
+
 }
 
 # Состояния
@@ -82,12 +79,14 @@ async def load_characters_f():
         characters[id_user].damage += damage
         characters[id_user].armor += armor
 
+
 def vk_message(*args):
+
     if len(args) == 2:
         vk.messages.send(
-            random_id = get_random_id(),
-            peer_id = args[1],
-            message = args[0]
+            random_id=get_random_id(),
+            peer_id=args[1],
+            message=args[0]
         )
     elif len(args) == 3:
         vk.messages.send(
@@ -98,33 +97,47 @@ def vk_message(*args):
         )
 
 
+def check_characters(message):
+    """Check  characters validate"""
+    if len(message) != 5:
+        return "Пожалуйста, введите данные по примеру, например bot воин 4 2 1", False
+    strength = message[2]
+    agility = message[3]
+    intelligence = message[4]
+    if strength.isdigit() and agility.isdigit() and intelligence.isdigit():
+        if int(strength) + int(agility) + int(intelligence) != 7:
+            return "Сумма характеристик должна быть равна 7, пожалуйста введите характеристики через пробел, например 4 2 1", False
+        return "Отлично, вы правильно ввели характеристики!", True
+    return "Пожалуйста, введите числами характеристики, например bot воин 4 2 1", False
+
+
 async def bot_cycle():
     load_characters = True
     while True:
         # try:
-            if load_characters == True:
+            if load_characters:
                 load_characters = False
                 await load_characters_f()
             for event in longpoll.listen():
                 if event.type == VkBotEventType.MESSAGE_NEW:
-                    if event.message.text.lower() == "!help" and event.message.from_id not in condition:
+                    if event.message.text.lower() == "help" and event.message.from_id not in condition:
                         peer_id = event.object.message['peer_id']
                         vk_message(configure_texts.help(), peer_id)
-                    elif event.message.text.lower() == "!создать персонажа" and event.message.from_id not in condition:
+                    elif event.message.text.lower() == "создать персонажа" and event.message.from_id not in condition:
                         peer_id = event.object.message['peer_id']
                         vk_message(configure_texts.create_hero(), peer_id)
                         condition[event.message.from_id] = "создание персонажа"
-                    elif event.message.text.lower() == "!мой персонаж" and event.message.from_id not in condition:
+                    elif event.message.text.lower() == "мой персонаж" and event.message.from_id not in condition:
                         mes_char = await db.show_character(event.message.from_id)
                         vk_message(mes_char, event.object.message['peer_id'])
-                    elif event.message.text.lower() == "!бой" and event.message.from_id not in condition:
+                    elif event.message.text.lower() == "бой" and event.message.from_id not in condition:
                         condition[event.message.from_id] = "бой"
-                        mobs[event.message.from_id] = monsters.Goblin("гоблин", 1)
-                        vk_message(attack(mobs[event.message.from_id].name), event.object.message["peer_id"], fight_keyboard)
-                    elif event.message.text.lower() == "!удалить персонажа" and event.message.from_id not in condition:
+                        gob = monsters.Goblin("гоблин", 1)
+                        vk_message(attack(gob.name), event.object.message["peer_id"], fight_keyboard)
+                    elif event.message.text.lower() == "удалить персонажа" and event.message.from_id not in condition:
                         del_message = await db.delete_character(event.message.from_id)
                         vk_message(del_message, event.object.message["peer_id"])
-                    elif event.message.text.lower() == "!предметы" and event.message.from_id not in condition and (event.message.from_id == 176803261 or event.message.from_id == admin):
+                    elif event.message.text.lower() == "предметы" and event.message.from_id not in condition and (event.message.from_id == 176803261 or event.message.from_id == admin):
                         vk.messages.send(
                             random_id=random.randint(1, random_number_message),
                             peer_id=event.object.message['peer_id'],
@@ -136,17 +149,27 @@ async def bot_cycle():
                         condition[event.message.from_id] = "предметы"
                     # проверка состояний
                     elif event.message.from_id in condition and condition[event.message.from_id] == "создание персонажа":
-                        if event.message.text.lower() == "!выход":
+                        if event.message.text.lower() == "выход":
                             vk_message("Жаль что мы не поиграем", event.object.message["peer_id"])
                             condition.pop(event.message.from_id, event.object.message["peer_id"])
                             continue
+
                         char = event.message.text.split(" ")
-                        characters[event.message.from_id] = globals()[char[1]](char[0], int(char[2]), int(char[3]), int(char[4]))
+                        """В отдельной функции надо сделать"""
+                        if check_characters(char)[-1]:
+                            characters[event.message.from_id] = globals()[char[1]](char[0], int(char[2]), int(char[3]), int(char[4]))
+                        else:
+                            vk_message(check_characters(char)[0], event.object.message["peer_id"])
+                            continue
+
+                            # ВОЗМОЖНО ОШИБКА БУДЕТ, НАДО ПРОВЕРИТЬ. НЕ УДАЛЯТЬ ПОКА НЕ ПРОВЕРЕННО
+
+
                         mes = await db.create_character(characters[event.message.from_id], event.message.from_id)
                         vk_message(mes, event.object.message["peer_id"])
                         condition.pop(event.message.from_id)
                     elif event.message.from_id in condition and condition[event.message.from_id] == "предметы":
-                        if event.message.text.lower() == "!выход":
+                        if event.message.text.lower() == "выход":
                             vk.messages.send(
                                 random_id=random.randint(1, random_number_message),
                                 peer_id=event.object.message['peer_id'],
@@ -162,18 +185,17 @@ async def bot_cycle():
                             message="Предмет добавлен",
                         )
                     elif event.message.from_id in condition and condition[event.message.from_id] == "бой":
-                        if mobs[event.message.from_id].health <= 0:
-                            vk_message(configure_texts.monster_defeat(mobs[event.message.from_id].name), event.object.message["peer_id"])
+                        if gob.health <= 0:
+                            vk_message(configure_texts.monster_defeat(gob.name), event.object.message["peer_id"])
                             condition.pop(event.message.from_id)
                             continue
                         else:
                             if event.message.text.lower() == "атака":
                                 hero_damage = characters[event.message.from_id].attack()
-                                monster_damage = mobs[event.message.from_id].attack()
-                                vk_message(hero_attack(mobs[event.message.from_id].name, hero_damage), event.object.message['peer_id'])
-                                vk_message(monster_attack(mobs[event.message.from_id].name, monster_damage), event.object.message['peer_id'], fight_keyboard)
-                                mobs[event.message.from_id].health -= hero_damage
-
+                                monster_damage = gob.attack()
+                                vk_message(hero_attack(gob.name, hero_damage), event.object.message['peer_id'])
+                                vk_message(monster_attack(gob.name, monster_damage), event.object.message['peer_id'], fight_keyboard)
+                                gob.health -= hero_damage
                                 continue
 
 
